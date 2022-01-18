0f4797efc270cdadb9d3162bcbe05d01a5be8753
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2022-01-18 11:57:49 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,14 @@
 // página de listagem
 import Pagination from "../../components/Pagination";
+import {BASE_URL} from "utils/requests"
+import axios from "axios"
 // import MovieStars from "components/MovieStars";
 // import MovieScore from "components/MovieScore";
 import MovieCard from "../../components/MovieCard";
+//FORMA ERRADA
+axios.get(`${BASE_URL}/movies?size=12&page=0`).then(response=>{
+    console.log(response.data)
+})
 function Listing() {
     return (
         <>
